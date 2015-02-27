38fc504e3c7e9613ace23ec58927f597a083ee8e
Nick Mandel <nrmandel@ucdavis.edu>
2015-02-26 18:46:53 -0800

    hello js implementation, working facebook login

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-  
+  hello.init({
+    "facebook": "884342831617060"
+  })
+
+  hello.on('auth.login', function(auth) {
+    console.log('logged in with fb', auth.network);
+    hello('facebook').api('/me').then(function(r) {
+      $('#profile').append('<img src="'+ r.thumbnail +'" /> Hey '+r.name);
+    });
+    // hello.api('facebook' + ':me/albums', function(r) {
+    //   if (!r || r.error) {
+    //     console.log('err opening albums');
+    //     return;
+    //   }
+    //   else if (!r.data || r.data.length === 0) {
+    //     console.log('no albums');
+    //     return;
+    //   }
+      
+    // })
+  })
+
+  $('#login').click(function() {
+    console.log('connecting to fb');
+    hello('facebook').login();
+  })
+
 })
