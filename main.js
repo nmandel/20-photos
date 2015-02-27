63792706fa61eee0493c225c7457a73edead0d8b
Nick Mandel <nrmandel@ucdavis.edu>
2015-02-26 23:10:58 -0800

    fixes bug where page wouldnt load on initial login

--- main.js
+++ main.js
@@ -2,41 +2,53 @@ $(document).ready(function() {
 
   hello.init({
     "facebook": "884342831617060"
-  }, {scope: "files, photos"})
+  }, {
+    scope: "files, photos",
+    redirect_uri: "redirect.html"
+  })
 
   hello.on('auth.login', function(auth) {
-    console.log('logged in with fb', auth.network);
+    console.log('logging in with fb', auth.network);
     hello('facebook').api('/me').then(function(r) {
-      $('#your-name').text('Welcome ' + r.name);
+      console.log(r.name + " is logged in");
+      setupSlideshow();
+      // $('#your-name').text('Welcome ' + r.name);
     });
     
   })
 
   $('#login').click(function() {
-    getPhotos();
+    hello('facebook').login();
     $('#btn').hide();
+    setupSlideshow()
+  });
+
+  function setupSlideshow() {
+    getPhotos();
     // fotorama needs to be called after all of the photos are loaded in
     setTimeout(function() {
       $('.fotorama').fotorama();
     }, 750);
-  });
+  }
 
   function getPhotos () {
     console.log('connecting to fb');
-    hello('facebook').login();
-    hello('facebook').api('me/photos', 'get', {limit: 20, width:1000, height: 1000}).then(function(r) {
-      console.log('data', r);
-      if (!r || r.error) {
-        console.log('err opening photos');
-        return;
-      }
-      else if (!r.data || r.data.length === 0) {
-        console.log('no photos');
-        return;
-      }
-      for (var i = 0; i < r.data.length; i++) {
-        $('.fotorama').append('<img src="'+ r.data[i].source +'" />');
-      }
+    hello('facebook').login({
+      force: false
+    }, function(auth) {
+      hello('facebook').api('me/photos', 'get', {limit: 20, width:1000, height: 1000}).then(function(r) {
+        if (!r || r.error) {
+          console.log('Error opening photos');
+          return;
+        }
+        else if (!r.data || r.data.length === 0) {
+          console.log('No photos available');
+          return;
+        }
+        for (var i = 0; i < r.data.length; i++) {
+          $('.fotorama').append('<img src="'+ r.data[i].source +'" />');
+        }
+      })
     })
   }
 
